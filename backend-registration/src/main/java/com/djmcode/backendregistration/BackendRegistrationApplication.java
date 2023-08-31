721e1039656a26e675f323e85c6ddcf5a5a7c594
Jerry Malcaire Djimafo <jerrymalcaire.djimafo@governikus.de>
2023-08-31 15:05:44 +0200

    project structure

---
+++ backend-registration/src/main/java/com/djmcode/backendregistration/BackendRegistrationApplication.java
@@ -0,0 +1,15 @@
+package com.djmcode.backendregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRegistrationApplication
+{
+
+  public static void main(String[] args)
+  {
+    SpringApplication.run(BackendRegistrationApplication.class, args);
+  }
+
+}
---
+++ backend-registration/src/test/java/com/djmcode/backendregistration/BackendRegistrationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.djmcode.backendregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRegistrationApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
